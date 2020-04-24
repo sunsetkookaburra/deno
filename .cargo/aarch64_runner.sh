23561472977906c6b23330fa5cc079987d7861fc
Oliver Lenehan <55334808+sunsetkookaburra@users.noreply.github.com>
2020-04-25 08:06:17 +1000

    correct substitution

--- .cargo/aarch64_runner.sh
+++ .cargo/aarch64_runner.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
-echo "${$@/\/home\/runner\/work\/deno\/deno\//}" >> /home/runner/work/deno/deno/.cargo/aarch64_tests.txt
+#!/bin/bash
+echo "${@/\/home\/runner\/work\/deno\/deno\//}" >> /home/runner/work/deno/deno/.cargo/aarch64_tests.txt
